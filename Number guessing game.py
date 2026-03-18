768297011d16fdf24d70e564caef7691c9a04cb9
jaideeps12 <jaideep345r@gmail.com>
2026-03-19 00:28:52 +0530

    updated the program

--- Number guessing game.py
+++ Number guessing game.py
@@ -1,1 +1,20 @@
-print("Hello there")
+import random
+print("Welcome to the number guessing game")
+print("You will have 6 choices")
+Lower_value=int(input("Enter the lower limit of the numbers: "))
+Upper_value=int(input("Enter the upper limit of the numbers: "))
+num=random.randint(Lower_value,Upper_value)
+n=0
+cc=6
+while n<cc:
+    n=n+1
+    guess=int(input("Enter your choice"))
+    if guess==num:
+        print("Yippe,you got it")
+        break
+    elif n>=cc and guess!=num:
+        print("The number was",num,"Better luck net time")
+    elif guess>num:
+        print("The number is high, but you are close")
+    elif guess<num:
+        print("The number is low but too close")
